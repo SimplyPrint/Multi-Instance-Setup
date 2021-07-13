089cc7de3961dbf63408c62f6622025e6bdc0d6e
math045b <math045b@gmail.com>
2021-07-13 13:12:25 +0200

    Added curl and removed unnecessary sudo

--- simply_instace.sh
+++ simply_instace.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker pi
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
-sudo pip3 install docker-compose
+sh get-docker.sh
+usermod -aG docker pi
+apt-get install libffi-dev libssl-dev
+apt install python3-dev
+apt-get install -y python3 python3-pip
+pip3 install docker-compose
+
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/main/docker-compose.yaml -o docker-compose.yaml
 docker-compose up
