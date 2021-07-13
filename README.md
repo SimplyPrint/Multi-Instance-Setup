28743ffe54f6182a7a5c7db3fcac981322971b10
math045b <math045b@gmail.com>
2021-07-13 12:03:11 +0200

    Removed curls, clone git instead

---
+++ README.md
@@ -0,0 +1,3 @@
+# simplyprint_multi_instance
+Run this comand to install 4 instances
+git clone https://github.com/math045b/simplyprint_multi_instance.git && cd simplyprint_multi_instance && sudo sh simply-instance.sh
--- simply_instace.sh
+++ simply_instace.sh
@@ -6,7 +6,4 @@ sudo apt-get install libffi-dev libssl-dev
 sudo apt install python3-dev
 sudo apt-get install -y python3 python3-pip
 sudo pip3 install docker-compose
-
-#Get 4 printer configuration
-curl https://raw.githubusercontent.com/math045b/simplyprint_multi_instance/main/docker-compose.yaml?token=AO6ZQMSJ42NDDQRIQEPJSOTA5VNFA -o docker-compose.yaml
 docker-compose up
