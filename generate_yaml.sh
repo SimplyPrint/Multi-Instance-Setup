9bf636818eee5a14f60fd455626776572e095738
math045b <math045b@gmail.com>
2021-07-15 11:41:38 +0200

    Generate yaml file

---
+++ generate_yaml.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+if [ -e docker-compose.yaml ]; then
+	rm docker-compose.yaml
+fi
+cat > docker-compose.yaml <<-EOL
+version: '2.4'
+
+services:
+EOL
+index=-1
+for device in $@; do
+	((index+=1))
+	cat >> docker-compose.yaml <<-EOL
+  sp${index}:
+    image: octoprint/octoprint
+    restart: unless-stopped
+
+    ports:
+      - 8${index}:80
+
+    devices:
+      - /dev/$device:/dev/$device
+
+    volumes:
+      - ./sp${index}:/octoprint
+
+	EOL
+done
