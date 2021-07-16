b9c9525856a46b00ac875eeed92f8274c1dcb94c
math045b <62756914+math045b@users.noreply.github.com>
2021-07-16 11:57:58 +0200

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yes | apt-get update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker pi
@@ -7,10 +8,10 @@ apt install python3-dev
 apt-get install -y python3 python3-pip
 pip3 install docker-compose
 
-curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/b1cf1385da6e26231a11be2238c6a004fb1046ee/check_devices.sh -o check_devices.sh
-curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/b1cf1385da6e26231a11be2238c6a004fb1046ee/functions.sh -o functions.sh
-curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/b1cf1385da6e26231a11be2238c6a004fb1046ee/generate_yaml.sh -o generate_yaml.sh
-curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/b1cf1385da6e26231a11be2238c6a004fb1046ee/get_device_id.sh -o get_device_id.sh
-curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/b1cf1385da6e26231a11be2238c6a004fb1046ee/instance_setup.sh -o instance_setup.sh
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/check_devices.sh -o check_devices.sh
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/functions.sh -o functions.sh
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/generate_yaml.sh -o generate_yaml.sh
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/get_device_id.sh -o get_device_id.sh
+curl https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/instance_setup.sh -o instance_setup.sh
 
 bash instance_setup.sh
