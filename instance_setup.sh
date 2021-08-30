f07ffde593bdc9250f3281537337bca46f8cf5e8
math045b <62756914+math045b@users.noreply.github.com>
2021-08-30 16:13:49 +0200

    Update instance_setup.sh

--- instance_setup.sh
+++ instance_setup.sh
@@ -117,64 +117,7 @@ read -n 1 -s -r -p "Press any key when all USB cables are removed ..."
 
 ## før
 
-sep="# ---------------------------------- #"
-printf "\nHow many SimplyPrint instances do you wish to set up?"
-
-read addAmount
-newAmount=$(($amount + $addAmount))
-
-. functions.sh
-
-# Get ports now that all are removed;
-get_ports
-last_total_ports=$total_ports
-
-for ((i = $amount ; i < $newAmount ; i++)); do
-  printf "\n\n\n\n"
-  echo $sep
-  echo "- Printer $i setup"
-
-  if [ $i -gt "1" ]; then
-    echo " !! Do NOT remove any USB cables !! "
-  fi
-
-  echo "Insert the USB cable into the pi (cable must be connected to the printer, and printer must be turned on)"
-  printf "\n"
-
-  read -n 1 -s -r -p "Press any key when cable is inserted... "
-  
-  last_ports=$return_ports # save last ports
-  get_ports                # get ports now
-
-  if [ "$total_ports" -lt "$last_total_ports" ] || [ "$total_ports" -eq "$last_total_ports" ]; then
-    printf "\n\n\n # !!! Failed; has same amount of ports as before. Either previous USB was plugged out, or new one wasn't inserted / registered\n\n"
-    exit
-  fi
-
-  last_total_ports=$total_ports # set new 'last'
-  this_port=""
-
-  for x in $return_ports; do
-    if [[ ! " $last_ports " =~ " $x " ]]; then
-      # This (new) value doesn't exist in old array; is the new device
-      this_port=$x
-    fi
-  done
-
-  if [ "$this_port" == "" ]; then
-    # Could not find a new port...
-    printf "\n\n\n # !!! Failed; New device not detected, try again - maybe with a different cable? Make sure the printer's power supply is turned on.\n\n"
-    echo "" >sp.config
-    exit
-  else
-    # Found port! Let's continue
-    printf "\n\nDevice $this_port detected!\n"
-    dev_id=$(bash get_device_id.sh $this_port)
-    echo "spDevices[$i]=$dev_id,$this_port" >>sp.config
-  fi
-
-done
-sed -i "s/total=[0-99]/total=${newTotal}/gI" sp.config
+add_instance.sh
 
 ## efter
 
