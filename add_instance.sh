d3e03c407e97fddeb752c2992fab69f68b3a33ea
math045b <62756914+math045b@users.noreply.github.com>
2021-09-03 19:12:33 +0200

    Update add_instance.sh

--- add_instance.sh
+++ add_instance.sh
@@ -61,7 +61,7 @@ for ((i = $total ; i < $newTotal ; i++)); do
 
 done
 
-bash generate_yaml.sh
+. generate_yaml.sh
 
 printf "\nHere are the links for octoprint, they will be offline until Docker is ready\n"
 ip=$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
