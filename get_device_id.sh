9035b580ff62b8790e972092dcc2ceeec9d59261
math045b <62756914+math045b@users.noreply.github.com>
2021-09-03 12:50:08 +0200

    Update get_device_id.sh

--- get_device_id.sh
+++ get_device_id.sh
@@ -5,6 +5,7 @@ ai=$(udevadm info $1)
 #echo "Getting ID from ${cut0::7}"
 cut1="${ai#*ID_VENDOR_ID=}"
 cut2="${ai#*ID_MODEL_ID=}"
-id="${cut1::4}:${cut2::4}"
+cut3="${ai#*ID_REVISION=}"
+id="${cut1::4}:${cut2::4}:${cut3::4}"
 
 echo $id
