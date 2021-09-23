af6471309c48485542781b0501b7b8914b796220
math045b <62756914+math045b@users.noreply.github.com>
2021-09-23 18:57:05 +0200

    Update add_instance.sh

--- add_instance.sh
+++ add_instance.sh
@@ -55,7 +55,7 @@ for ((i = $total ; i < $newTotal ; i++)); do
   else
     # Found port! Let's continue
     printf "\n\nDevice $this_port detected!\n"
-    printf "Added to config: spDevices[$i]=$dev_id,$this_port"
+    printf "Added to config: spDevices[$i]=$dev_id,$this_port\n\n"
     dev_id=$(bash get_device_id.sh $this_port)
     echo "spDevices[$i]=$dev_id,$this_port" >>sp.config
   fi
