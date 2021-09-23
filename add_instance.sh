29930e5f25d9eb61950c44a0b77d3be0a36613b2
math045b <62756914+math045b@users.noreply.github.com>
2021-09-23 20:16:30 +0200

    Update add_instance.sh

--- add_instance.sh
+++ add_instance.sh
@@ -62,8 +62,6 @@ for ((i = $total ; i < $newTotal ; i++)); do
 
 done
 
-. generate_yaml.sh
-
 printf "\nHere are the links for octoprint, they will be offline until Docker is ready\n"
 ip=$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 echo "ip: $ip"
@@ -72,3 +70,5 @@ for ((i = $total ; i < $newTotal ; i++)); do
 done
 
 sed -i "s/total=[0-99]/total=${newTotal}/gI" sp.config
+
+. generate_yaml.sh
