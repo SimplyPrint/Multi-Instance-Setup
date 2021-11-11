0735fa39d00a78f069f474470cee92eeebe91162
math045b <62756914+math045b@users.noreply.github.com>
2021-11-11 13:32:46 +0100

    Create cron_check.sh

---
+++ cron_check.sh
@@ -0,0 +1,11 @@
+# Do it 5 times every 10 seconds (spans out over 50s)
+bash check_devices.sh
+sleep 10
+bash check_devices.sh
+sleep 10
+bash check_devices.sh
+sleep 10
+bash check_devices.sh
+sleep 10
+bash check_devices.sh
+sleep 10
