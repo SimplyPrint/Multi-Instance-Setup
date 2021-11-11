ad81f855b47e4cb60db8cac6c3a9c939062477dd
math045b <62756914+math045b@users.noreply.github.com>
2021-11-11 11:05:07 +0100

    Update get_device_id.sh

--- get_device_id.sh
+++ get_device_id.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 ai=$(udevadm info $1)
-cut0="${ai#*MAJOR=}"
-cut1="${ai#*ID_VENDOR_ID=}"
-cut2="${ai#*ID_MODEL_ID=}"
-cut3="${ai#*ID_REVISION=}"
+
+## Other
+#cut0="${ai#*MAJOR=}"
+#cut1="${ai#*ID_VENDOR_ID=}"
+#cut2="${ai#*ID_MODEL_ID=}"
+#cut3="${ai#*ID_REVISION=}"
 #cut4="${ai#*ID_USB_DRIVER=}" # :${cut4::7}
+#id="${cut0::3}:${cut1::4}:${cut2::4}:${cut3::4}"
+
 # For Prusa
-#cut5="${ai#*ID_SERIAL_SHORT=}" # :${cut5::18}
-id="${cut0::3}:${cut1::4}:${cut2::4}:${cut3::4}" # :${cut5::18}"
+cut5="${ai#*ID_SERIAL_SHORT=}"
+id="${cut5::19}"
 
 echo $id
