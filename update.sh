93a7c7195dccdf258ebbdd900979d3f5db614981
math045b <62756914+math045b@users.noreply.github.com>
2021-11-29 15:04:20 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 
 echo "$(date -u) - update.sh" >>"$(pwd)"/logs/scripts.log
 
-baseUrl="https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/"
+baseUrl="https://raw.githubusercontent.com/SimplyPrint/Multi-Instance-Setup/main/"
 
-curl ${baseUrl}main/check_devices.sh -o check_devices.sh
-curl ${baseUrl}main/functions.sh -o functions.sh
-curl ${baseUrl}main/generate_yaml.sh -o generate_yaml.sh
-curl ${baseUrl}main/get_device_id.sh -o get_device_id.sh
-curl ${baseUrl}main/instance_setup.sh -o instance_setup.sh
-curl ${baseUrl}main/add_instance.sh -o add_instance.sh
-curl ${baseUrl}main/cron_check.sh -o cron_check.sh
-curl ${baseUrl}main/log_controller.sh -o log_controller.sh
-curl ${baseUrl}main/update.sh -o update.sh
+curl ${baseUrl}check_devices.sh -o check_devices.sh
+curl ${baseUrl}functions.sh -o functions.sh
+curl ${baseUrl}generate_yaml.sh -o generate_yaml.sh
+curl ${baseUrl}get_device_id.sh -o get_device_id.sh
+curl ${baseUrl}instance_setup.sh -o instance_setup.sh
+curl ${baseUrl}add_instance.sh -o add_instance.sh
+curl ${baseUrl}cron_check.sh -o cron_check.sh
+curl ${baseUrl}log_controller.sh -o log_controller.sh
+curl ${baseUrl}update.sh -o update.sh
